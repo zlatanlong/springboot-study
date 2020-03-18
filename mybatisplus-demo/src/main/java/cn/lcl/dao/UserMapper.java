c683e9e61b8ffe69899de9d90c55a4d798a6afbd
Zlatanlong <928339761@qq.com>
2020-03-18 11:28:26 +0800

    UpdateTest DeleteTest

--- mybatisplus-demo/src/main/java/cn/lcl/dao/UserMapper.java
+++ mybatisplus-demo/src/main/java/cn/lcl/dao/UserMapper.java
@@ -1,11 +1,24 @@
 package cn.lcl.dao;
 
 import cn.lcl.entity.User;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
 public interface UserMapper extends BaseMapper<User> {
+
+    List<User> selectAll(@Param(Constants.WRAPPER) Wrapper<User> wrapper);
+
+
+    Page<User> selectUserPage(Page<User> page, @Param(Constants.WRAPPER) Wrapper<User> wrapper);
+
 }
---
+++ mybatisplus-demo/src/test/java/cn/lcl/UpdateTest.java
@@ -0,0 +1,113 @@
+package cn.lcl;
+
+import cn.lcl.dao.UserMapper;
+import cn.lcl.entity.User;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.conditions.update.LambdaUpdateChainWrapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class UpdateTest {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Test
+    public void updateByIds() {
+        User user = new User();
+        user.setAge(27);
+        user.setEmail("wangtianff@qq.com");
+        user.setId(1088248166370832385L);
+
+        int rows = userMapper.updateById(user);
+
+        System.out.println("影响记录数" + rows);
+
+    }
+
+
+    /**
+     * DEBUG==>  Preparing: UPDATE user SET age=?, email=? WHERE (name = ? AND age = ?)
+     * DEBUG==> Parameters: 29(Integer), liyiwei@qq.com(String), 李艺伟(String), 28(Integer)
+     * DEBUG<==    Updates: 1
+     * 影响记录数1
+     */
+    @Test
+    public void updateByWrapper() {
+        UpdateWrapper<User> updateWrapper = Wrappers.update();
+
+        updateWrapper.eq("name", "李艺伟").eq("age", 28);
+        User user = new User();
+        user.setAge(29);
+        user.setEmail("liyiwei@qq.com");
+
+        int rows = userMapper.update(user, updateWrapper);
+
+        System.out.println("影响记录数" + rows);
+
+    }
+
+    @Test
+    public void updateByWrapper2() {
+        User user = new User();
+        user.setAge(30);
+        user.setEmail("liyiwei@qq.com");
+
+        UpdateWrapper<User> updateWrapper = Wrappers.update(user);
+
+        User newUser = new User();
+        newUser.setAge(29);
+
+        int rows = userMapper.update(newUser, updateWrapper);
+
+        System.out.println("影响记录数" + rows);
+
+    }
+
+    @Test
+    public void updateByWrapper3() {
+        UpdateWrapper<User> updateWrapper = Wrappers.update();
+
+        updateWrapper.eq("name", "李艺伟").eq("age", 28)
+                .set("age", 30);
+
+        int rows = userMapper.update(null, updateWrapper);
+
+        System.out.println("影响记录数" + rows);
+
+    }
+
+    @Test
+    public void updateByWrapperLambda() {
+        LambdaUpdateWrapper<User> lambdaUpdate = Wrappers.lambdaUpdate();
+
+        lambdaUpdate.eq(User::getName, "李艺伟")
+                .eq(User::getAge, 29)
+                .set(User::getAge, 31);
+
+        int rows = userMapper.update(null, lambdaUpdate);
+
+        System.out.println("影响记录数" + rows);
+
+    }
+
+    @Test
+    public void updateByWrapperLambdaChain() {
+
+        boolean b = new LambdaUpdateChainWrapper<User>(userMapper)
+                .eq(User::getName, "李艺伟")
+                .eq(User::getAge, 31)
+                .set(User::getAge, 32)
+                .update();
+
+        System.out.println("是否成功" + b);
+
+    }
+
+
+}
