9090a4aa469bbacd396062a4861623f9d8612c9c
Zlatanlong <928339761@qq.com>
2020-03-19 11:22:08 +0800

    add sb-swagger2

---
+++ sb-02-swagger2/src/main/java/com/sblearning/swagger2/entity/User.java
@@ -0,0 +1,34 @@
+package com.sblearning.swagger2.entity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel("用户")
+public class User {
+
+    @ApiModelProperty("用户名")
+    public String username;
+    @ApiModelProperty("密码")
+    public String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
