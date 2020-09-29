9a6928f8af37f6addc384e2346953cd434d15ba7
zlatanlong <928339761@qq.com>
2020-09-29 17:36:02 +0800

    websocket-text

---
+++ sb-06-websocket/src/main/java/top/lclong/sb06websocket/Sb06WebsocketApplication.java
@@ -0,0 +1,13 @@
+package top.lclong.sb06websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb06WebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb06WebsocketApplication.class, args);
+    }
+
+}
---
+++ sb-06-websocket/src/test/java/top/lclong/sb06websocket/Sb06WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lclong.sb06websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb06WebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
