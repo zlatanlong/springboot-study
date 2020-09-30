6b7f458cedd883c37cb15233e2fac15b1df0795b
zlatanlong <928339761@qq.com>
2020-09-30 16:49:32 +0800

    redis

---
+++ sb-07-redis/src/test/java/top/lclong/sb07redis/Sb07RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lclong.sb07redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb07RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
